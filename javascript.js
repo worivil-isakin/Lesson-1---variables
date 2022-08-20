b34c325f27bd84bb8857c8164d1c02216a0dd890
ErinCleaver-GRCC <77419665+ErinCleaver-GRCC@users.noreply.github.com>
2022-08-20 04:27:19 -0400

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,9 @@
+// A gobal varabile is declared with var 
+var x = 10;
+
+// A local varabile is declared with let.
+let x = 5;
+
+// A varabile that will not change is declared with const
+const pie = 3.14159265
+
